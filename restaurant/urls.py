edbddf0274dd91a629e85e382a74856fb2d05496
EyobABN <eyob.alamerew.belay@gmail.com>
2023-12-27 12:04:30 +0300

    Implement menu and booking APIs

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('menu', views.MenuView.as_view(), name='menu'),
+    path('menu/<int:pk>', views.MenuItemView.as_view(), name='menuitem'),
+    path('booking', views.BookingView.as_view(), name='booking'),
+    path('booking/<int:pk>', views.BookingItemView.as_view(), name='bookingitem')
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
+from rest_framework import generics
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class MenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.filter()
+    serializer_class = MenuSerializer
+
+class BookingView(generics.ListCreateAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+class BookingItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Booking.objects.filter()
+    serializer_class = BookingSerializer
